c5c397fef7d52c79efab0f5be385fa6e2037ab53
Ankit Pal <anpal@altimetrik.com>
2024-01-19 18:02:21 +0530

    get hotels details through feignClient

--- UserService/src/main/java/com/user/micro/service/external/services/HotelService.java
+++ UserService/src/main/java/com/user/micro/service/external/services/HotelService.java
@@ -1,8 +1,6 @@
 package com.user.micro.service.external.services;
-
 import com.user.micro.service.entities.Hotel;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
